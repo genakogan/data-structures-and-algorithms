d2cbc05fbec806985b75565959e926e73bc5e58e
GenadyKogan <rgkogan@gmail.com>
2022-08-14 12:28:49 +0300

    add: titles

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -1,22 +1,44 @@
-import React, {ReactElement} from 'react';
-import Container from './Container';
-import LabelText from './LabelText';
-import Row from '../Common/Row';
+import React, { ReactElement } from "react";
+import Container from "./Container";
+
+import Row from "../Common/Row";
+import Titles from "../Common/Titles";
 
 /*interface NavbarProps {
     changeTheme: Function;
   }*/
 
 const TopMenu: React.FC = (): ReactElement => {
-    return (
-        <Container>
-          <LabelText>Animation and visualization of data structure</LabelText>
-          <Row justifyContent="space-between" margin="0 18px">
-           
-          </Row>
-        </Container>
-      );
-
+  return (
+    <Container>
+      <Titles fontSize="24px" underline="" cursor="">
+        Animation and visualization of data structure
+      </Titles>
+      <Row justifyContent="space-between" margin="0 24px">
+        <Titles
+          fontSize="15px"
+          underline="text-decoration: underline"
+          cursor="pointer"
+        >
+          Help
+        </Titles>
+        <Titles
+          fontSize="15px"
+          underline="text-decoration: underline"
+          cursor="pointer"
+        >
+          Team
+        </Titles>
+        <Titles
+          fontSize="15px"
+          underline="text-decoration: underline"
+          cursor="pointer"
+        >
+          About
+        </Titles>
+      </Row>
+    </Container>
+  );
 };
 
 export default TopMenu;
