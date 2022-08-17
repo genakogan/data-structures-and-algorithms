1eb4be15d1046e9884f21925d87c355022f55763
GenadyKogan <rgkogan@gmail.com>
2022-08-17 16:31:16 +0300

    add: Window

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import "./App.css";
+// Genady Kogan
 import React, { ReactElement, useState } from "react";
 import Home from "./components/Home/Home";
 import { ThemeProvider } from "styled-components";
 import themes from "./themes";
+import Theme from "./models/Theme";
+
+// Window for Help Team About on 
+import WindowHTA from "./components/TopMenu/WindowHTA/WindowHTA";
+
+
+
 
 const App: React.FC<{}> = (): ReactElement => {
   /*
@@ -11,12 +18,19 @@ const App: React.FC<{}> = (): ReactElement => {
    anywhere beneath it in the component tree,
   via the context API.
   */
-  const [globalTheme, setGlobalTheme] = useState<string>("dark");
+  const [globalTheme, setGlobalTheme] = useState<Theme>(themes.light);
+  const [isWindowVisible, setWindowVisible] = useState<boolean>(false);
 
-  {/*<Home toggleTheme={setGlobalTheme}  ></Home>*/}
+  const onWindowExit = () => {
+    setWindowVisible(false);
+  };
+  const onWindowOpen = () => {
+    setWindowVisible(true);
+  };
   return (
-    <ThemeProvider theme={globalTheme === "dark" ? themes.dark : themes.light}>
-      <Home  ></Home>
+    <ThemeProvider theme={globalTheme}>
+      <Home changeTheme={setGlobalTheme} onTeamClick={onWindowOpen}></Home>
+      <WindowHTA isVisible={isWindowVisible} onExit={onWindowExit}></WindowHTA>
     </ThemeProvider>
   );
 };
