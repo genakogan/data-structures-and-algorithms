1e54fa5684dfd4bc1e0765cc4760ca9d0bac7089
ghgkogan <rgkogan@gmail.com>
2022-08-17 23:55:55 +0300

    add size to model

--- src/components/TopMenu/WindowHTA/WindowHTA.tsx
+++ src/components/TopMenu/WindowHTA/WindowHTA.tsx
@@ -1,17 +1,20 @@
 // Genady Kogan
-import React, {ReactElement} from 'react';
-import Modal from './Modal/Modal';
+import React, { ReactElement } from "react";
+import Modal from "./Modal/Modal";
 
-interface Props{
-    isVisible: boolean;
-    onExit: ()=>void;
+interface Props {
+  isVisible: boolean;
+  onExit: () => void;
 }
 
-const WindowHTA: React.FC<Props>=(props: Props): ReactElement=>{
-    return(
-        <Modal isVisible={props.isVisible} onExit = {props.onExit}>
-        <div>dff</div>
-        </Modal>
-    );
+const WindowHTA: React.FC<Props> = (props: Props): ReactElement => {
+  return (
+    <Modal isVisible={props.isVisible} onExit={props.onExit}>
+      <div style={{ width: "600px", height: "500px" }}>
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </Modal>
+  );
 };
 export default WindowHTA;
