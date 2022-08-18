a787a1dbc80af201634c369ce899918ed2945bf3
GenadyKogan <rgkogan@gmail.com>
2022-08-18 15:54:29 +0300

    add: control indecator location

--- src/components/TopMenu/Window/NikaCarousel/NikaCarousel.tsx
+++ src/components/TopMenu/Window/NikaCarousel/NikaCarousel.tsx
@@ -1,6 +1,9 @@
+
 import { ReactElement } from "react";
+import Row from "../../../Common/Row";
 import Column from "./Column";
 import Container from "./Container";
+import ControlIndecators from "./ControlIndecators/ControlIndecators";
 import SideContainer from "./SideContainer";
 
 interface Props {
@@ -11,7 +14,15 @@ const NikaCarousel: React.FC<Props> = (props: Props): ReactElement => {
   return <Container>
     <SideContainer direction = "left"></SideContainer>
     <Column width="100%">
+    <Row justifyContent="center" style={{height: '10%', width: '100%'}}>
+          <ControlIndecators
+           
+          ></ControlIndecators>
+    </Row>
+    <Row justifyContent="center" style={{height: '90%', width: '100%'}}>
     {props.children}
+        </Row>
+  
     </Column>
     <SideContainer direction = "right"></SideContainer>
     </Container>;
