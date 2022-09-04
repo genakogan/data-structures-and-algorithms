32f9280b4aad5d21745a580f19d716b41d0b8da4
GenadyKogan <rgkogan@gmail.com>
2022-09-05 00:15:37 +0300

    add: add node options on canvas

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,19 +1,30 @@
-import React, { ReactElement, useRef } from "react";
+import React, { ReactElement, useRef, useState } from "react";
 import TreeNode from "../TreeNode/TreeNode";
 import CanvasContainer from "./CanvasContainer";
 
-interface Props{
+interface Props {
+  adjacencyList: Array<Array<number>>;
+  nodeKeys: Array<string>;
   zoomPercentage: number;
-
+  addNewNode: () => void;
 }
 
 const Canvas: React.FC<Props> = (props: Props): ReactElement => {
   const canvasRef = useRef<HTMLDivElement>(null);
+  const adjacencyList = props.adjacencyList;
+
   return (
     <CanvasContainer ref={canvasRef}>
-      <TreeNode canvasRef={canvasRef} zoomPercentage={props.zoomPercentage} isActive={true} content="1"></TreeNode>
-
-      <TreeNode canvasRef={canvasRef} zoomPercentage={props.zoomPercentage} isActive={true} content="2"></TreeNode>
+      {adjacencyList.map((val: Array<number>, index: number) => {
+        return (
+          <TreeNode
+            canvasRef={canvasRef}
+            zoomPercentage={props.zoomPercentage}
+            isActive={true}
+            content="1"
+          ></TreeNode>
+        );
+      })}
     </CanvasContainer>
   );
 };
