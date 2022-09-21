e9e297669cf6989f56e430e86d3319088682ec75
GenadyKogan <rgkogan@gmail.com>
2022-09-21 15:41:58 +0300

    add: speed visualization

--- src/components/Tree/Algorithms/dfs.ts
+++ src/components/Tree/Algorithms/dfs.ts
@@ -5,26 +5,44 @@ let globalVisited: Array<number> = [];
 const dfs = async (
   graph: Array<Array<number>>,
   node: number,
-  setVisited: Function
+  setVisited: Function,
+  visualizationSpeed: number,
+  previousNode: number
 ): Promise<void> => {
+
+  
   if (globalVisited.includes(node)) return;
-  await helpers.asyncTimout(1000);
+  await helpers.asyncTimout(visualizationSpeed);
+
+  /*
+  the concat() method joins two or more strings, does
+  not change the existing strings, returns a new string.
+  */
   globalVisited = globalVisited.concat(node);
   setVisited(globalVisited);
-  console.log(globalVisited);
+
 
   for (let neighbour of graph[node]) {
-    await dfs(graph, neighbour, setVisited);
+    await dfs(
+      graph,
+      neighbour,
+      setVisited,
+      visualizationSpeed,
+      node
+    );
   }
 };
 
 const dfsWrapper = async (
   graph: Array<Array<number>>,
   node: number,
-  setVisited: Function
+  setVisited: Function,
+  visualizationSpeed: number,
 ) => {
   globalVisited = [];
-  await dfs(graph, node, setVisited);
+
+  /* last parametrs is previousNode, -1 for starting */
+  await dfs(graph, node, setVisited, visualizationSpeed, -1);
 };
 
 export default dfsWrapper;
