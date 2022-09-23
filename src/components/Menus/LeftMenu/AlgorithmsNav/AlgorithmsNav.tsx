a23242230e0a6e263329183d365b0dbe1cc46d3c
GenadyKogan <rgkogan@gmail.com>
2022-09-23 11:22:47 +0300

    add: create undirected edge from left menu

--- src/components/Menus/LeftMenu/AlgorithmsNav/AlgorithmsNav.tsx
+++ src/components/Menus/LeftMenu/AlgorithmsNav/AlgorithmsNav.tsx
@@ -1,6 +1,9 @@
+// Genady Kogan
 import React, { ReactElement, useState } from "react";
+import Dropdown from "../../../Common/Dropdown/Dropdown";
 import Row from "../../../Common/Row";
-import Titles from "../../../Common/Titles";
+import StyledButton from "../../../Tree/CreateEdgeModal/StyledButton";
+import LeftMenuContentText from "../LeftMenuContentText";
 import AlgorithmsNavContainer from "./AlgorithmsNavContainer";
 import Arrow from "./Arrow";
 import ClearButtonContainer from "./NodeOptions/ClearButtonContainer";
@@ -17,10 +20,15 @@ interface Props {
   addNewNode: () => void;
   clearCanvas: Function;
   onUndirectedEdgeClick: Function;
+  adjacencyList: Array<Array<number>>;
+  onAddEdge: Function;
 }
 
 const AlgorithmsNav: React.FC<Props> = (props: Props): ReactElement => {
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [firstNode, setFirstNode] = useState<number>(0);
+  const [secondNode, setSecondNode] = useState<number>(1);
+
   const toggleVisibility = () => {
     setIsVisible((prev) => !prev);
   };
@@ -29,18 +37,11 @@ const AlgorithmsNav: React.FC<Props> = (props: Props): ReactElement => {
       <ToggleButton isVisible={isVisible} onClick={() => toggleVisibility()}>
         <Arrow isVisible={isVisible}></Arrow>
       </ToggleButton>
+
       <Row justifyContent="space-evenly" margin="10px 0px">
-        <Titles
-          fontSize="18px"
-          underline=""
-          cursor=""
-          userSelect=""
-          marginLeft=""
-          marginRight=""
-        >
-          Node options
-        </Titles>
+        <LeftMenuContentText>Node options</LeftMenuContentText>
       </Row>
+
       <Row justifyContent="space-evenly" margin="20px 0px">
         <OptionButton tooltipContent="Add node" onClick={props.addNewNode}>
           <AddIcon></AddIcon>
@@ -70,18 +71,40 @@ const AlgorithmsNav: React.FC<Props> = (props: Props): ReactElement => {
             props.clearCanvas();
           }}
         >
-          <Titles
-            fontSize="18px"
-            underline=""
-            cursor=""
-            userSelect=""
-            marginLeft=""
-            marginRight=""
-          >
-            Clear canvas
-          </Titles>
+          <LeftMenuContentText>Clear canvas</LeftMenuContentText>
         </ClearButtonContainer>
       </Row>
+
+      <Row justifyContent="space-evenly" margin="10px 0px">
+        <LeftMenuContentText>Create undirected edge</LeftMenuContentText>
+      </Row>
+
+      <Row justifyContent="space-between">
+        <LeftMenuContentText>From</LeftMenuContentText>
+        <Dropdown
+          content={props.adjacencyList.map(
+            (_, index: number) => `${index + 1}`
+          )}
+          selectedTile={firstNode}
+          setSelectedTile={setFirstNode}
+        />
+        <LeftMenuContentText>To</LeftMenuContentText>
+        <Dropdown
+          content={props.adjacencyList.map(
+            (_, index: number) => `${index + 1}`
+          )}
+          selectedTile={secondNode}
+          setSelectedTile={setSecondNode}
+        />
+      </Row>
+
+      <StyledButton
+        onClick={() => {
+          props.onAddEdge(firstNode, secondNode);
+        }}
+      >
+        Add
+      </StyledButton>
     </AlgorithmsNavContainer>
   );
 };
